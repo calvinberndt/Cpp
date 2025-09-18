c613278a6f0efcc4d3b8e09c608864e6ffd628d1
Calvin Berndt <calvinwberndtjr@gmail.com>
2025-09-18 14:17:24 -0500

    Enhance loops.cpp: Add input handling for positive integers and star printing loop

--- projects/chapter4/loops.cpp
+++ projects/chapter4/loops.cpp
@@ -11,9 +11,54 @@ int main() {
         numbers[i] = i;
         cout << "Iteration " << i << endl;
     }
+
+    //Iterate through the list and print each element
     for (int v: numbers) {
         cout << v << " ";
     }
+
+    cout << endl;
+    cout << endl;
+
+    // //Using EOF to terminate input
+    // int sum = 0;
+    // int num;
+
+    // //Loop including initialization
+
+    // cout << "Enter numbers to sum, and press Ctrl+D (or Ctrl+Z on Windows) to end input: ";
+    // while (cin >> num) {
+    //     sum += num;
+    //     cout << "Enter the next integer: "; //update
+    // }
+    // //output
+    // cout << "The total sum is: " << sum << endl;
+
+    //Create a program that requires a positive integer > 0 as input
+    //Program will terminate when the user enters an end-of-file 
+
+    unsigned int value;
+
+    cout << "Enter a positive integer (or press Ctrl+D/Ctrl+Z to end): ";
+    while (cin >> value) { 
+        if (value > 0) {
+            cout << "You entered: " << value << endl;
+            // Create a loop that prints a star
+            for (unsigned int i = 1; i < value + 1; i++) {
+
+                // cout << string(i, '*') << endl;
+
+                for (unsigned int j = 1; j <= i; j++) {
+                    cout << "*";
+                }
+                cout << endl;
+            }
+        } else {
+            cout << "Please enter a positive integer greater than zero." << endl;
+        }
+        cout << "Enter a positive integer (or press Ctrl+D/Ctrl+Z to end): ";
+    }
+
 
     return 0;
 }
