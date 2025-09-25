25783fad21f426b24ade92ba06ede2e735e474d1
Calvin Berndt <calvinwberndtjr@gmail.com>
2025-09-25 12:33:33 -0500

    Add initial project files for chapter 6 including .gitignore, launch.json, settings.json, tasks.json, and main.cpp

---
+++ projects/chapter5/binarySearch/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+#include <vector>
+int binarySearch(vector<int>& numbers, int target);
+
+int main() {
+    cout << "Hello, World!" << endl;
+    return 0;
+}
+
+
+int binarySearch(vector<int>& numbers, int target) {
+    int indexofTarget = -1;
+    int left = 0;
+    int right = numbers.size() - 1;
+
+    
+
+
+}
