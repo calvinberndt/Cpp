6b5cfa1766408c572701a5e015b15c4c5d6e2596
Calvin Berndt <calvinwberndtjr@gmail.com>
2025-09-25 13:48:56 -0500

    Implement code changes to enhance functionality and improve performance

--- projects/chapter7/main.cpp
+++ projects/chapter7/main.cpp
@@ -1,8 +1,40 @@
 #include <iostream>
 using namespace std;
-int main() {
-    
 
+class HelloWorld {
+public:
+
+    void greet() {
+        cout << "Hello, World!" << endl;
+    }
+
+    void privateGreeting(){
+        string name;
+        int age;
+        cout << "Enter your name and age: ";
+        cin >> name >> age;
+        cout << "This is a public method." << endl;
+        cout << "Name: " << name << ", Age: " << age << endl;
+    }
+
+    void exposeSecret() {
+        secretMethod();
+    }
+
+private:
+    void secretMethod() {
+        cout << "This is a private method." << endl;
+    }
     
+};
+
+int main() {
+
+    HelloWorld hw;
+    hw.greet();
+    hw.privateGreeting();
+    hw.exposeSecret();
     return 0;
+
 }
+
