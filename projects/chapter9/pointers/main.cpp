5e68d0332746b2acdd60133479aa29accf445fbd
Calvin Berndt <calvinwberndtjr@gmail.com>
2025-10-14 13:09:31 -0500

    Enhance main.cpp with detailed examples of references and pointers, including constant pointers and their usage

--- projects/chapter9/pointers/main.cpp
+++ projects/chapter9/pointers/main.cpp
@@ -26,6 +26,10 @@ int main() {
 
 
 
+    cout << "Fundamental Differences between reference and pointers:" << endl;
+    cout << "------------------------" << endl;
+    //Using references
+
     int a = 10;
     int& rA = a; //reference variable
 
@@ -35,14 +39,46 @@ int main() {
     cout << "Value of rA: " << rA << endl;
     cout << "Value of using &rA: " << &rA << endl;
 
-
+    cout << "The usage of Pointers:" << endl;
+    cout << "------------------------" << endl;
     //Using pointers
     int* pA = &a; //pointer variable
 
-    cout << "Value of pA: " << pA << endl;
-    cout << "Value of using *pA: " << *pA << endl;
-    cout << "Value of using &pA: " << &pA << endl;
+    cout << "Value of pA: " << pA << endl; //address of variable a
+    cout << "Value of using *pA: " << *pA << endl; //dereferencing pointer to get value
+    cout << "Value of using &pA: " << &pA << endl; //address of pointer variable
 
 
+
+    //The useage of CONSTANT with Pointers:
+    cout << "------------------------" << endl;
+    cout << "The useage of CONSTANT with Pointers:" << endl;
+    int b = 20;
+    const int* pB = &b; //pointer to a constant integer
+    //This means that the value being pointed to by pB cannot be changed through pB
+    //However, pB can be reassigned to point to another variable
+    cout << "Value of using *pB: " << *pB << endl;
+    // *pB = 30; //Error: cannot change value of a constant through pointer
+    int c = 30;
+    pB = &c; //Valid: pointer can be reassigned to point to another variable
+    cout << "Value of using *pB after reassignment: " << *pB << endl;
+
+
+    //vs Constant pointer
+    cout << "------------------------" << endl;
+    cout << "The useage of CONSTANT POINTER:" << endl;
+    int d = 40;
+    int* const pC = &d; //constant pointer to an integer
+    //This treats pC as a reference to the variable d
+    //This means that pC cannot be reassigned to point to another variable
+    cout << "Value of using *pC: " << *pC << endl;
+    *pC = 50; //Valid: value being pointed to can be changed
+    cout << "Value of using *pC after changing value: " << *pC << endl;
+    // int e = 60;
+    // pC = &e; //Error: cannot reassign a constant pointer
+    cout << "Value of d after changing value through pC: " << d << endl;
+    cout << "Value of using &pC: " << &pC << endl; //address of constant pointer variable
+    cout << "Value of using &d: " << &d << endl; //address of variable d
     return 0;
+
 }
