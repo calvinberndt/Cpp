77b317cb08cb888330d737925c16a2e3b9e15e95
Calvin Berndt <calvinwberndtjr@gmail.com>
2025-10-14 14:09:00 -0500

    exercise largest Float

--- projects/chapter9/pointers/exercise.cpp
+++ projects/chapter9/pointers/exercise.cpp
@@ -1,11 +1,33 @@
 #include <iostream>
 using namespace std;
 
+//pass by pointer and return by pointer.
+float* findLargest(float* num1, float* num2, float*num3){
+
+    float largestFloat = *num1;
+    float* pt;
+
+    if(largestFloat < *num2){
+        largestFloat = *num2;
+    }
+    if(largestFloat < *num3){
+        largestFloat = *num3;
+    };
+
+
+
+    return pt = &largestFloat;
+}
 
 int main() {
 
+    float num1 = 2.1;
+    float num2 = 2.5;
+    float num3 = 3.8;
 
-    
+
+    float largestInt = *findLargest(&num1, &num2, &num3);
+    cout << "The largest float is: " << largestInt << endl;
 
     return 0;
 }
